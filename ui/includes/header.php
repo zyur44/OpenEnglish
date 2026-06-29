823ce8fdc48b0f1a63f35b093f9032488e5a44d4
Dương Minh Nhân <duongminhnhan09@gmail.com>
2026-06-29 10:54:48 +0700

    change nav bar language to vnese

--- ui/includes/header.php
+++ ui/includes/header.php
@@ -2,10 +2,10 @@
         <nav>
             <ul>
                 <li><a href="home.php"><img src="../frontend/assets/image/logo/logo_placeholder.png" width="75px"></a></li>
-                <li><a href="home.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="my-courses.php">Course</a></li>
-                <li><a href="login.php">Login</a></li>
+                <li><a href="home.php">Trang chủ</a></li>
+                <li><a href="about.php">Giới thiệu</a></li>
+                <li><a href="my-courses.php">Khóa học</a></li>
+                <li><a href="login.php">Đăng nhập</a></li>
             </ul>
         </nav>
     </header>
