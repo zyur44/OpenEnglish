3689c3a0ad717842b6c95d50f72138d073116207
Dương Minh Nhân <duongminhnhan09@gmail.com>
2026-07-02 12:23:13 +0700

    Update footer

--- ui/includes/footer.php
+++ ui/includes/footer.php
@@ -1,4 +1,4 @@
     <!-- ===== Chân trang của website ===== -->
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> My Website. All rights reserved.</p>
+        <p>&copy; <?php echo date("Y"); ?> Group 11 Website. All rights reserved.</p>
     </footer>
